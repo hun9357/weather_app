6bfe45e465f54221e577aee99929197cdb44ef7b
james kim <hun935701@gmail.com>
2023-08-07 15:09:06 -0500

    basic design

--- App.js
+++ App.js
@@ -1,13 +1,49 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react"
-import {  View  } from 'react-native';
+import {  View, StyleSheet, Text  } from 'react-native';
 
 export default function App() {
   return (
-    <View style= {{flex: 1, flexDirection: 'row'}}>
-      <View style = {{flex: 1, backgroundColor: 'red'}}></View>
-      <View style = {{flex: 1, backgroundColor: 'teal'}}></View>
-      <View style = {{flex: 1, backgroundColor: 'orange'}}></View>
+    <View style = {styles.container}>
+      <View style = {styles.city}>
+        <Text style={styles.cityName}>
+          Seoul
+        </Text>
+      </View>
+      <View style = {styles.weather}>
+        <View style = {styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style = {styles.desc}> Sunny </Text>
+        </View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1, backgroundColor:'tomato'
+  },
+  city:{
+    flex:1, justifyContent: "center",alignItems:"center"
+  },
+  cityName:{
+    color:"black", fontSize: 68, fontWeight: "500"
+  },
+  weather:{
+    flex:3,
+  },
+  day:{
+    flex: 1,
+    alignItems:"center"
+  },
+  temp:{
+    fontSize: 168,
+    marginTop: 50
+  },
+  desc:{
+    marginTop: -30,
+    fontSize: 60
+  }
+})
 
